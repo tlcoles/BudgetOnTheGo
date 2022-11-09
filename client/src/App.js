f3e4e1112b09438ca0face5033acf7117c9e2f41
nirmeet-baweja <nirmeet.baweja@gmail.com>
2022-11-09 20:59:36 +0000

    refactor app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import Home from './pages/Home';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import Home from "./pages/Home";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
-function App() {
+const App = () => {
   return (
     <ChakraProvider>
-      <Home name="BudgetOnTheGo user"/>
+      <Home name="BudgetOnTheGo user" />
     </ChakraProvider>
-  )
-}
+  );
+};
 
 export default App;
