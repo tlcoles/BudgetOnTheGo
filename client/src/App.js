90f53cd0384a2bac351a6e292b2f46b7c1b07c06
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-09 23:02:48 +0100

    extend chakra theme with custom colors

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { budgetTheme } from './styles/theme';
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={budgetTheme}>
       <Home name="BudgetOnTheGo user"/>
     </ChakraProvider>
   )
