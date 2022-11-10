c038d0a81ce10c67c11d09edf6525f0d6231b26b
HenniePenny <h.kressner@hotmail.de>
2022-11-10 20:33:11 +0100

    remove budgeTheme for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import { budgetTheme } from './styles/theme';
 
 const App = () => {
   return (
-    <ChakraProvider theme={budgetTheme}>
-      <Home name="BudgetOnTheGo user"/>
+    <ChakraProvider>
+      <Home name="BudgetOnTheGo user" />
     </ChakraProvider>
   );
 };
