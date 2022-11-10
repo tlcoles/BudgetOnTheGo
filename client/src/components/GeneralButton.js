303f2a1815d0cc9a49268c0064f920768bf462a8
tornicke <torniqe@gmail.com>
2022-11-10 17:39:14 +0100

    Refactoring button settings

--- client/src/components/GeneralButton.js
+++ client/src/components/GeneralButton.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { GeneralButton } from "@chakra-ui/react";
 
-const GeneralButton = () => {
+const GeneralButton = (props) => {
   return (
     <div>
       <button type="button">ButtonTitle</button>
