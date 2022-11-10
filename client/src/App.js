3bd40e075e8bc994a4828ca29aeda812832d9e94
tornicke <torniqe@gmail.com>
2022-11-10 17:35:45 +0100

    Refactoring the button settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import Home from './pages/Home';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import Home from "./pages/Home";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import GeneralButton from "./components/GeneralButton";
 
 function App() {
   return (
     <ChakraProvider>
-      <Home name="BudgetOnTheGo user"/>
+      <Home name="BudgetOnTheGo user" />
+      <GeneralButton /> //! TODO Take this out after you're done testing
     </ChakraProvider>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/components/GeneralButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GeneralButton = () => {
+  return (
+    <div>
+      <button type="button">ButtonTitle</button>
+    </div>
+  );
+};
+
+export default GeneralButton;
