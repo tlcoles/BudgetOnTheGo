c39c4a7d02c125983b3003c6e0d9bdc95c40c779
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-10 19:48:14 +0100

    import Navbar component, add Navbar to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "./components/Navbar"
+import { budgetTheme } from './styles/theme';
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={budgetTheme}>
       <Navbar />
       <Home name="BudgetOnTheGo user" />
     </ChakraProvider>
