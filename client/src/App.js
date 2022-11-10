8b4be8a0d4c49cf29a4146cc9eac7790e050dc15
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-10 19:43:00 +0100

    insert Chakra.ui Navbar component, with basic links

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
     <ChakraProvider>
+      <Navbar />
       <Home name="BudgetOnTheGo user" />
     </ChakraProvider>
   );
