aaf6277d1addc4870ff022161b34bf1916218946
HenniePenny <h.kressner@hotmail.de>
2022-11-10 19:55:57 +0100

    refactor input field to controlled input field with form control

--- client/src/components/inputField.js
+++ client/src/components/inputField.js
@@ -1,8 +1,25 @@
 import React from "react";
-import { Input } from "@chakra-ui/react";
+import {
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
 
-const InputField = () => {
-  return <Input focusBorderColor="#86BBD8" placeholder="Sample placeholder" />;
+const InputField = (props) => {
+  return (
+    <>
+      <FormControl isRequired>
+        <FormLabel>{props.placeholder}</FormLabel>
+        <Input
+          focusBorderColor="#86BBD8"
+          type="email"
+          placeholder={props.placeholder}
+        />
+      </FormControl>
+    </>
+  );
 };
 
 export default InputField;
