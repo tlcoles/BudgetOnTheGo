e7c1e60eca91898a27f1cd88e7eb385cc51d5ced
HenniePenny <h.kressner@hotmail.de>
2022-11-10 21:53:35 +0100

    add value and handleChange

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import InputField from "./components/inputField";
 
 const App = () => {
+  const [value, setValue] = React.useState("");
+  const handleChange = (event) => setValue(event.target.value);
+
   return (
     <ChakraProvider>
       <Home name="BudgetOnTheGo user" />
+      <InputField
+        label="Email"
+        type="email"
+        placeholder="example@email.com"
+        value={value}
+        onChange={handleChange}
+      />
     </ChakraProvider>
   );
 };
