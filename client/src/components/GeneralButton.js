6a06671e3bab54d8dc55a4a7b2d68f0a9935ef05
tornicke <torniqe@gmail.com>
2022-11-10 22:35:28 +0100

    Added button props

--- client/src/components/GeneralButton.js
+++ client/src/components/GeneralButton.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-const GeneralButton = () => {
-  return <Button colorScheme="blue">ButtonTitle</Button>;
+const GeneralButton = (props) => {
+  console.log(props);
+  return (
+    <Button
+      colorScheme="blue"
+      color="#F9F0F6"
+      minWidth="20%"
+      onClick={props.handleClick}
+    >
+      {props.buttonTitle}
+    </Button>
+  );
 };
 
 export default GeneralButton;
