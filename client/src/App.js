e98363412e972fa92a82b2ad9840819f658454ba
HenniePenny <h.kressner@hotmail.de>
2022-11-11 19:29:27 +0100

    deleted testing code from App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,11 @@ import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { budgetTheme } from "./styles/theme";
-import InputField from "./components/inputField";
 
 const App = () => {
-  const [value, setValue] = React.useState("");
-  const [email, setEmail] = React.useState("");
-
   return (
     <ChakraProvider theme={budgetTheme}>
       <Home name="BudgetOnTheGo user" />
-      <InputField
-        label="Email"
-        type="email"
-        placeholder="example@email.com"
-        value={value}
-        setValue={setValue}
-      />
-      <InputField
-        label="Password"
-        type="password"
-        placeholder="example2@email.com"
-        value={email}
-        setValue={setEmail}
-      />
     </ChakraProvider>
   );
 };
