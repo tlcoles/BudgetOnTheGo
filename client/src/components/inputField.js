ba14ed6abbec0d95fd178aa99dce33016288f62b
HenniePenny <h.kressner@hotmail.de>
2022-11-11 19:28:54 +0100

    added comments to inputField.js for usage in parent element

--- client/src/components/inputField.js
+++ client/src/components/inputField.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Input, FormControl, FormLabel } from "@chakra-ui/react";
 
+//comment for usage in parent element is below
+
 const InputField = (props) => {
   const handleChange = (event) => props.setValue(event.target.value);
 
@@ -23,3 +25,36 @@ const InputField = (props) => {
 };
 
 export default InputField;
+
+{
+  /*
+  !Usage in the parent element demonstrated here with App.js:
+  const App = () => {
+  !Use this to set the state for the needed props
+  const [email, setValue] = React.useState("");
+  const [pw, setEmail] = React.useState("");
+
+  return (
+    <ChakraProvider theme={budgetTheme}>
+      <Home name="BudgetOnTheGo user" />
+      <InputField
+        label="Email"
+        type="email"
+        placeholder="example@email.com"
+        value={email}
+        setValue={setValue}
+      />
+      <InputField
+        label="Password"
+        type="password"
+        placeholder="examplePassword"
+        value={pw}
+        setValue={setEmail}
+      />
+    </ChakraProvider>
+  );
+};
+
+!read more here, if needed: https://chakra-ui.com/docs/components/form-control
+  */
+}
