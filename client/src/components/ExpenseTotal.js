134e94bcd9b03208035678bc3c2a2486182c4c92
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-12 17:04:57 +0100

    create test sample for ExpenseTotal

---
+++ client/src/components/ExpenseTotal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Badge } from "@chakra-ui/react";
+
+const ExpenseTotal = () => {
+  return (
+    <Badge colorScheme="red">
+      Spent today: 1000
+    </Badge>
+  );
+};
+
+export default ExpenseTotal;
