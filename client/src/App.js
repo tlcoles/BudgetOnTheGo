96f25b5e5840fa93e25be1cfc171549c79351c9a
tornicke <torniqe@gmail.com>
2022-11-12 10:56:32 +0100

    Reverting to the "main" version of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,11 @@ import Home from "./pages/Home";
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { budgetTheme } from "./styles/theme";
-import GeneralButton from "./components/GeneralButton";
 
 const App = () => {
-  const handleLogin = (event) => {
-    console.log("loginClicked");
-    console.log(event.target);
-  };
   return (
     <ChakraProvider theme={budgetTheme}>
       <Home name="BudgetOnTheGo user" />
-      <div>
-        <GeneralButton buttonTitle="Log in" handleClick={handleLogin} />
-      </div>
-      <div>
-        <GeneralButton buttonTitle="Sign up" />
-      </div>
-      <div>
-        <GeneralButton buttonTitle="Submit" />
-      </div>
     </ChakraProvider>
   );
 };
