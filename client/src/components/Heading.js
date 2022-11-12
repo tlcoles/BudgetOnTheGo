70e3f70ce1834a714e6a5a29f4505399623b3864
HenniePenny <h.kressner@hotmail.de>
2022-11-12 15:29:50 +0100

    added Chakra UI heading for component

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -1,7 +1,14 @@
+import { Heading } from "@chakra-ui/react";
 import React from "react";
 
-const Heading = () => {
-  return <div></div>;
+const HeadingH1Component = () => {
+  return (
+    <div>
+      <Heading as="h1" mb={4} color={secondary}>
+        Budget On The Go
+      </Heading>
+    </div>
+  );
 };
 
-export default Heading;
+export default HeadingH1Component;
