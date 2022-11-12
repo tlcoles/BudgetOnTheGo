b4487ece79991ac0757ef87cc947a22e9fa36fbf
HenniePenny <h.kressner@hotmail.de>
2022-11-12 19:39:14 +0100

    add 2 input fields, dropdown and submit button

--- client/src/pages/AddExpense.js
+++ client/src/pages/AddExpense.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { Select } from "@chakra-ui/react";
+import InputField from "../components/InputField";
+import GeneralButton from "../components/GeneralButton";
 
 const AddExpensePage = () => {
-  return <div></div>;
+  const [expenseItem, setExpenseItem] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [value, setValue] = React.useState("");
+
+  const submit = () => {
+    console.log(expenseItem);
+    // in here we're not gonna clg the values bu actually make request to the api to log us in
+  };
+
+  return (
+    <div>
+      <InputField
+        label={"Expense Item"}
+        value={expenseItem}
+        type={"text"}
+        placeholder={"Breakfast"}
+        setValue={setExpenseItem}
+      />
+      <InputField
+        label={"Amount in €"}
+        value={amount}
+        type={"number"}
+        placeholder={"5.00 €"}
+        setValue={setAmount}
+      />
+      <Select
+        placeholder="Select a category"
+        focusBorderColor={"highlight"}
+        variant="outline"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      >
+        <option value="Food & Drink">Food & Drink</option>
+        <option value="Travel">Travel</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Home">Home</option>
+        <option value="Other">Other</option>
+      </Select>
+      <GeneralButton buttonTitle={"Submit"} handleClick={submit} />
+    </div>
+  );
 };
 
 export default AddExpensePage;
