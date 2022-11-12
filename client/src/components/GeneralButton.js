71e015919daf308c182ca8c2e6ce2e8e8f28fc51
tornicke <torniqe@gmail.com>
2022-11-12 10:57:06 +0100

    Moving the individual button details in Button.js

--- client/src/components/GeneralButton.js
+++ client/src/components/GeneralButton.js
@@ -9,11 +9,38 @@ const GeneralButton = (props) => {
       backgroundColor={"button"}
       color={"primary"}
       minWidth="10%"
+      my={2}
       onClick={props.handleClick}
     >
       {props.buttonTitle}
     </Button>
   );
 };
+
+// import GeneralButton from "./components/GeneralButton";
+
+// const App = () => {
+//   const handleLogin = (event) => {
+//     console.log("loginClicked");
+//     console.log(event.target);
+//   };
+//   return (
+//     <ChakraProvider theme={budgetTheme}>
+//       <Home name="BudgetOnTheGo user" />
+//       <div>
+//         <GeneralButton buttonTitle="Log in" handleClick={handleLogin} />
+//       </div>
+//       <div>
+//         <GeneralButton buttonTitle="Sign up" handleClick={handleSignUp} />
+//       </div>
+//       <div>
+//         <GeneralButton buttonTitle="Submit" handleClick={handleSubmit} />
+//       </div>
+//       <div>
+//         <GeneralButton buttonTitle="Apply" handleClick={handleApply} />
+//       </div>
+//     </ChakraProvider>
+//   );
+// };
 
 export default GeneralButton;
