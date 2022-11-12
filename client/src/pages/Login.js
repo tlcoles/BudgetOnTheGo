6f1069fa0b74b0c0087536776133af5825567656
tornicke <torniqe@gmail.com>
2022-11-12 12:18:19 +0100

    Incorporating components into Login.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GeneralButton from "../components/GeneralButton";
+import InputField from "../components/InputField";
+
+const LoginPage = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const login = () => {
+    console.log(username, password);
+    // in here we're not gonna clg the values bu actually make request to the api to log us in
+  };
+
+  return (
+    <div>
+      <InputField
+        label={"Username"}
+        value={username}
+        type={"text"}
+        placeholder={"Username"}
+        setValue={setUsername}
+      />
+      <InputField
+        label={"Password"}
+        value={password}
+        type={"password"}
+        placeholder={"Password"}
+        setValue={setPassword}
+      />
+      <GeneralButton buttonTitle={"Log in"} handleClick={login} />
+    </div>
+  );
+};
+
+export default LoginPage;
