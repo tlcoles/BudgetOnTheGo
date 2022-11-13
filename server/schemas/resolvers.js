498aab9c9ac133ee4b2047c2e46651b9b1271ab7
HenniePenny <h.kressner@hotmail.de>
2022-11-13 15:50:46 +0100

    added resolvers outline (empty)

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const resolvers = {
+  Query: {
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
+  },
+
+  Mutation: {},
+};
+
+module.exports = resolvers;
