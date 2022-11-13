8a425be6fa9258e1656086bfad91a48cd7f2eede
tornicke <torniqe@gmail.com>
2022-11-13 12:58:14 +0100

    Adding heading to the Settings page

---
+++ client/src/components/HeadingSettings.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const HeadingSettings = () => {
+  return (
+    <Heading as="h1" mt={4} mb={4} color={"secondary"}>
+      Account Information
+    </Heading>
+  );
+};
+
+export default HeadingSettings;
