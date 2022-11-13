64cc6a46c2d3a192eb3bfefb42d96a930c5f3df5
HenniePenny <h.kressner@hotmail.de>
2022-11-13 11:30:37 +0100

    adding Router and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./pages/Home";
 import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ChakraProvider, Container } from "@chakra-ui/react";
 import { budgetTheme } from "./styles/theme";
 import Navbar from "./components/Navbar";
@@ -10,8 +11,12 @@ const App = () => {
     <ChakraProvider theme={budgetTheme}>
       <Navbar />
       <Container>
-        <Home name="BudgetOnTheGo user" />
-        <LoginPage />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home name="BudgetOnTheGo user" />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Routes>
+        </Router>
       </Container>
     </ChakraProvider>
   );
