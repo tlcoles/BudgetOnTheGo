9ba37f4488ab874d73dd99f454ece7ecb82ea62a
tornicke <torniqe@gmail.com>
2022-11-13 13:02:43 +0100

    Added dummy data to display the chart

--- client/src/pages/Chart.js
+++ client/src/pages/Chart.js
@@ -1,2 +1,18 @@
 import React from "react";
 import { PieChart } from "react-minimal-pie-chart";
+
+const defaultLabelStyle = {
+  fontSize: "5px",
+  fontFamily: "sans-serif",
+};
+
+function Chart() {
+  const data = [{ title: "One", value: 10, color: "#E38627" }];
+  return (
+    <div>
+      <PieChart data={data} />
+    </div>
+  );
+}
+
+export default Chart;
