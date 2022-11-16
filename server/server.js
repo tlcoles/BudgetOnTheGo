0760e66d77449600cce84a286b67a9309dc7fe7e
HenniePenny <h.kressner@hotmail.de>
2022-11-16 23:27:12 +0100

    create const server and create new instance of Apollo server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -17,8 +22,22 @@ if (process.env.NODE_ENV === "production") {
 
 app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}.`);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
-});
+};
+
+// db.once("open", () => {
+//   app.listen(PORT, () => {
+//     console.log(`Listening on PORT ${PORT}.`);
+//   });
+// });
