beb2961e6e76caf6fcb9c90ca02ff4e8bf00ca3d
HenniePenny <h.kressner@hotmail.de>
2022-11-16 23:51:10 +0100

    added addUser mutation for testing purposes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,12 @@
 const resolvers = {
-  Query: {},
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
