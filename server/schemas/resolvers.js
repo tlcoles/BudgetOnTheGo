297d36be146dceedcd8cc88ada04c017703e94e6
HenniePenny <h.kressner@hotmail.de>
2022-11-17 18:52:29 +0100

    exchanged test mutation for expenses query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,7 @@
 const resolvers = {
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
+  Query: {
+    expenses: async () => {
+      return Expense.find();
     },
   },
 };
