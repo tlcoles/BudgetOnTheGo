2aceb30d60d2701f41e42b2b644ad005d9b34e35
tornicke <torniqe@gmail.com>
2022-11-18 19:57:59 +0100

    Commenting out mutations for testing

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,57 +1,62 @@
-const { Expense } = require("../models");
+const { Expense, User } = require("../models");
 
 const resolvers = {
   Query: {
+    //! see expenses for a given logged in user
     expenses: async () => {
-      return Expense.find();
+      return await Expense.find({}).populate("user");
     },
+    users: async () => {
+      return await User.find({});
+    },
+    //! another query for the profile info (PASSWORD IS OPTIONAL)
+    //! OPTIONAL QUERY
   },
 
-  Mutation: {
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
+  // Mutation: {
+  //   addUser: async (parent, args) => {
+  //     const user = await User.create(args);
+  //     const token = signToken(user);
 
-      return {token, user};
-    },
-    updateUser: async (parent, args, context) => {
-      if (context.user) {
-        return User.findByIdAndUpdate(context.user.id, args, {
-          new: true,
-        });
-      }
-      throw new AuthenticationError("Not logged in");
-    },
+  //     return { token, user };
+  //   },
+  //   updateUser: async (parent, args, context) => {
+  //     if (context.user) {
+  //       return User.findByIdAndUpdate(context.user.id, args, {
+  //         new: true,
+  //       });
+  //     }
+  //     throw new AuthenticationError("Not logged in");
+  //   },
 
+  //   login: async (parent, { email, password }) => {
+  //     //! add email validation
+  //     const user = await User.findOne({ email });
 
-    login: async(parent, {email, password}) => {
-    //! add email validation
-      const user = await User.findOne({ email });
+  //     if (!user) {
+  //       throw new AuthenticationError("Incorrect credentials");
+  //     }
 
-      if (!user) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
+  //     const correctPw = await user.isCorrectPassword(password);
 
-      const correctPw = await user.isCorrectPassword(password);
+  //     if (!correctPw) {
+  //       throw new AuthenticationError("Incorrect credentials");
+  //     }
 
-      if (!correctPw) {
-        throw new AuthenticationError("Incorrect credentials");
-      }
+  //     const token = signToken(user);
+  //     return { token, user };
+  //   },
 
-      const token = signToken(user);
-      return {token, user};
-    },
+  //   addExpense: async (parent, { item, category, amount, userId }) => {
+  //     const expense = await Expense.create({ item, category, amount, userId });
 
-    addExpense: async (parent, {item, category, amount, userId }) => {
-      const expense = await Expense.create({item, category, amount, userId });
-
-      await User.findOneAndUpdate(
-        { _id: userId },
-        { $addToSet: { expenses: expense._id }}
-      );
-      return expense;
-    }
-  }
+  //     await User.findOneAndUpdate(
+  //       { _id: userId },
+  //       { $addToSet: { expenses: expense._id } }
+  //     );
+  //     return expense;
+  //   },
+  // },
 };
 
 module.exports = resolvers;
