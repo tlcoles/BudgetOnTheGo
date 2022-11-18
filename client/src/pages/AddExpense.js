2bcb6e0632b45057e44a1b5a044795705d857c6a
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-18 21:29:08 +0100

    rebuild with corrected components

--- client/src/pages/AddExpense.js
+++ client/src/pages/AddExpense.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Select } from "@chakra-ui/react";
+import { FormControl, FormLabel } from "@chakra-ui/react";
 import HeadingH1Component from "../components/Heading";
 import InputField from "../components/InputField";
 import GeneralButton from "../components/GeneralButton";
+import { Select } from "chakra-react-select";
 
 const AddExpensePage = () => {
   // const [item, setItem] = React.useState("");
@@ -31,79 +32,82 @@ const AddExpensePage = () => {
 
   const heading = "Add your expense";
 
-  export const categories = [
-    {value="Food & Drink", label: "Food & Drink", color: "#0052CC" },
+  const categories = [
+    { value: "Food & Drink", label: "Food & Drink", color: "#0052CC" },
     { value: "Travel", label: "Travel", color: "#5243AA" },
     { value: "Entertainment", label: "Entertainment", color: "#FF5630" },
     { value: "Home", label: "Home", color: "#FF5630" },
     { value: "Other", label: "Other", color: "#FF5630" },
   ];
 
-    // submit form
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-      console.log(formState);
-      try {
-        const { data } = await login({
-          variables: { ...formState },
-        });
-  
-        Auth.login(data.login.token);
-      } catch (error) {
-        console.error(error);
-      }
-  
-      // clear form values
-      setFormState({
-        username: "",
-        password: "",
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
       });
-    };
+
+      Auth.login(data.login.token);
+    } catch (error) {
+      console.error(error);
+    }
+
+    // clear form values
+    setFormState({
+      item: "",
+      amount: "",
+      value: "",
+    });
+  };
 
   return (
     <div>
       <HeadingH1Component heading={heading} />
-      <InputField
-        label={"Item"}
-        value={formState.item}
-        name={"item"}
-        type={"text"}
-        placeholder={"Breakfast"}
-        onChange={handleChange}
-      />
-      <InputField
-        label={"Amount"}
-        value={formState.amount}
-        name={"item"}
-        type={"text"}
-        placeholder={"5.00"}
-        onChange={handleChange}
-      />
-      <Select
-        name="categories"
-        placeholder="Select a category"
-        className="chakra-react-select"
-        classNamePrefix="chakra-react-select"
-        options={categories}
-        onChange={handleChange}
-        chakraStyles={{
-          dropdownIndicator: (provided) => ({
-            ...provided,
-            bg: "transparent",
-            px: 2,
-            cursor: "inherit",
-          }),
-          indicatorSeparator: (provided) => ({
-            ...provided,
-            display: "none",
-          }),
-        }}
-      />
-      <GeneralButton
-        onClick={handleChange}
-        type={"submit"}
-        buttonTitle={"Log in"}
-      />
+      <form onSubmit={handleFormSubmit}>
+        <InputField
+          label={"Item"}
+          value={formState.item}
+          name={"item"}
+          type={"text"}
+          placeholder={"Breakfast"}
+          onChange={handleChange}
+        />
+        <InputField
+          label={"Amount"}
+          value={formState.amount}
+          name={"amount"}
+          type={"number"}
+          placeholder={"5.00"}
+          onChange={handleChange}
+        />
+        <Select
+          name="categories"
+          placeholder="Select a category"
+          className="chakra-react-select"
+          classNamePrefix="chakra-react-select"
+          options={categories}
+          selectedOptionStyle="check"
+          chakraStyles={{
+            dropdownIndicator: (provided) => ({
+              ...provided,
+              bg: "transparent",
+              px: 2,
+              cursor: "inherit",
+            }),
+            indicatorSeparator: (provided) => ({
+              ...provided,
+              display: "none",
+            }),
+          }}
+        />
+        <GeneralButton
+          onClick={handleChange}
+          type={"submit"}
+          buttonTitle={"Submit"}
+        />
+      </form>
     </div>
   );
 };
