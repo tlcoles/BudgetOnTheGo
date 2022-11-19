f27bd22d12d3e04b6073ad5b9113e17fea8f1e13
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-11-19 09:29:58 +0100

    restore AddExpense to main version

--- client/src/pages/AddExpense.js
+++ client/src/pages/AddExpense.js
@@ -1,113 +1,52 @@
-import React, { useState } from "react";
-import { FormControl, FormLabel } from "@chakra-ui/react";
+import React from "react";
+import { Select } from "@chakra-ui/react";
 import HeadingH1Component from "../components/Heading";
 import InputField from "../components/InputField";
 import GeneralButton from "../components/GeneralButton";
-import { Select } from "chakra-react-select";
 
 const AddExpensePage = () => {
-  // const [item, setItem] = React.useState("");
-  // const [amount, setAmount] = React.useState("");
-  // const [value, setValue] = React.useState("");
+  const [item, setItem] = React.useState("");
+  const [amount, setAmount] = React.useState("");
+  const [value, setValue] = React.useState("");
 
-  // const submit = () => {
-  //   console.log(expenseItem);
-  //   // in here we're not gonna clg the values bu actually make request to the api to log us in
-  // };
-
-  const [formState, setFormState] = useState({
-    item: "",
-    amount: "",
-    value: "",
-  });
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
+  const submit = () => {
+    console.log(expenseItem);
+    // in here we're not gonna clg the values bu actually make request to the api to log us in
   };
 
   const heading = "Add your expense";
 
-  const categories = [
-    { value: "Food & Drink", label: "Food & Drink", color: "#0052CC" },
-    { value: "Travel", label: "Travel", color: "#5243AA" },
-    { value: "Entertainment", label: "Entertainment", color: "#FF5630" },
-    { value: "Home", label: "Home", color: "#FF5630" },
-    { value: "Other", label: "Other", color: "#FF5630" },
-  ];
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-    try {
-      const { data } = await login({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.login.token);
-    } catch (error) {
-      console.error(error);
-    }
-
-    // clear form values
-    setFormState({
-      item: "",
-      amount: "",
-      value: "",
-    });
-  };
-
   return (
     <div>
       <HeadingH1Component heading={heading} />
-      <form onSubmit={handleFormSubmit}>
-        <InputField
-          label={"Item"}
-          value={formState.item}
-          name={"item"}
-          type={"text"}
-          placeholder={"Breakfast"}
-          onChange={handleChange}
-        />
-        <InputField
-          label={"Amount"}
-          value={formState.amount}
-          name={"amount"}
-          type={"number"}
-          placeholder={"5.00"}
-          onChange={handleChange}
-        />
-        <Select
-          name="categories"
-          placeholder="Select a category"
-          className="chakra-react-select"
-          classNamePrefix="chakra-react-select"
-          options={categories}
-          selectedOptionStyle="check"
-          chakraStyles={{
-            dropdownIndicator: (provided) => ({
-              ...provided,
-              bg: "transparent",
-              px: 2,
-              cursor: "inherit",
-            }),
-            indicatorSeparator: (provided) => ({
-              ...provided,
-              display: "none",
-            }),
-          }}
-        />
-        <GeneralButton
-          onClick={handleChange}
-          type={"submit"}
-          buttonTitle={"Submit"}
-        />
-      </form>
+      <InputField
+        label={"Item"}
+        value={item}
+        type={"text"}
+        placeholder={"Breakfast"}
+        setValue={setItem}
+      />
+      <InputField
+        label={"Amount"}
+        value={amount}
+        type={"number"}
+        placeholder={"5.00"}
+        setValue={setAmount}
+      />
+      <Select
+        placeholder="Select a category"
+        focusBorderColor={"highlight"}
+        variant="outline"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      >
+        <option value="Food & Drink">Food & Drink</option>
+        <option value="Travel">Travel</option>
+        <option value="Entertainment">Entertainment</option>
+        <option value="Home">Home</option>
+        <option value="Other">Other</option>
+      </Select>
+      <GeneralButton buttonTitle={"Submit"} handleClick={submit} />
     </div>
   );
 };
