174de2cbb9fac68253d6afa13b02566cf6221dd1
HenniePenny <h.kressner@hotmail.de>
2022-11-20 12:07:21 +0100

    added virtuals for date handling

--- server/models/Expense.js
+++ server/models/Expense.js
@@ -1,32 +1,43 @@
 const { Schema, model } = require("mongoose");
 // const dateFormat = require("../utils/dateFormat"); //! 21-MERN ex 26 dateFormat.js is example
 
-const expenseSchema = new Schema({
-  item: {
-    type: String,
-    minlength: 1,
-    maxlength: 25,
-    required: true,
-    trim: true,
+const expenseSchema = new Schema(
+  {
+    item: {
+      type: String,
+      minlength: 1,
+      maxlength: 25,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // get: (timestamp) => dateFormat(timestamp),
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  category: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // get: (timestamp) => dateFormat(timestamp),
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+expenseSchema.virtual("formattedCreatedAt").get(function () {
+  return new Date(this.createdAt);
 });
 
 const Expense = model("Expense", expenseSchema);
