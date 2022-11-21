10d49136b35050e092d636d57e9ec9de58335ad9
HenniePenny <h.kressner@hotmail.de>
2022-11-21 12:40:44 +0100

    added sample toast for testing

---
+++ client/src/components/Toast.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, useToast } from "@chakra-ui/react";
+
+const ToastExample = () => {
+  const toast = useToast();
+  return (
+    <Button
+      onClick={() =>
+        toast({
+          title: "Account created.",
+          description: "We've created your account for you.",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        })
+      }
+    >
+      Show Toast
+    </Button>
+  );
+};
+
+export default ToastExample;
